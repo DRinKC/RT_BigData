b8dab9bd4103c0c31cee37a701f8fc409e675932
DR042460 <david.rodgers@cerner.com>
2016-08-30 16:56:44 -0500

    edited program to just count # of sentences and order them alphabetically

--- Lab1/Documentation/Lab1README.txt
+++ Lab1/Documentation/Lab1README.txt
@@ -1,12 +1,10 @@
 Input = array of strings:
 
 Array[0] = location of input file
-Array[1,2] = location of folders of output files (must not exist before executing program)
+Array[1] = location of folders of output files (must not exist before executing program)
 
 Input used:
 array[0] = "C:\Users\DR042460\Documents\GitHub\RT_BigData_Fall2016_Rodgers\Lab1\Source\testInput.txt"
-array[1] = "C:\Users\DR042460\Documents\GitHub\RT_BigData_Fall2016_Rodgers\Lab1\Source\FinalWordOutput" [word count]
-array[2] = "C:\Users\DR042460\Documents\GitHub\RT_BigData_Fall2016_Rodgers\Lab1\Source\FinalSentenceOutput" [sentence count]
-
+array[1] = "C:\Users\DR042460\Documents\GitHub\RT_BigData_Fall2016_Rodgers\Lab1\Source\FinalOutput" 
 
 NOTE: Jar file is too large to be uploaded. Directions do not specify to submit jar file, so PLEASE Email me at daryt7@mail.umkc.edu for more information. 
--- Lab1/Source/SparkWordCount.scala
+++ Lab1/Source/SparkWordCount.scala
@@ -11,19 +11,12 @@ object SparkWordCount {
     val sc = new SparkContext(sparkConf);
     val input = sc.textFile(args(0));
 
-    // word count per file
-    val wc = input.flatMap(line => line.split(" ")).map(word =>(word,1));
-    val preWordOutput = wc.reduceByKey(_ + _);
-    // switch key and value, sort by value, switch key and value back. sorted in ascending order
-    val wordOutput = preWordOutput.map(item => item.swap).sortByKey(true).map(item => item.swap);
-    wordOutput.saveAsTextFile(args(1));
-
-    // word count per sentence
-    val sentCount = input.flatMap(line => line.split('.')).map(sentence => (sentence, (sentence.split(" ")).length));
+    // count # of sentences
+    val sentCount = input.flatMap(line => line.split('.')).map(sentence => (sentence, 1);
     val preSentOutput = sentCount.reduceByKey(_+_);
-    // switch key and value, sort by value, switch key and value back. sorted in ascending order
-    val sentOutput = preSentOutput.map(item => item.swap).sortByKey(true).map(item => item.swap);
-    sentOutput.saveAsTextFile(args(2));
+    // sort alphabetically
+    val sentOutput = preSentOutput.sortByKey(true);
+    sentOutput.saveAsTextFile(args(1));
   }
 
 }
