55557f4e879ad1fe7054a9fcfe63ab25856e28e2
DR042460 <david.rodgers@cerner.com>
2016-08-30 16:05:01 -0500

    added word count per sentence portion

--- Lab1/Source/SparkWordCount.scala
+++ Lab1/Source/SparkWordCount.scala
@@ -10,11 +10,20 @@ object SparkWordCount {
     val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]");
     val sc = new SparkContext(sparkConf);
     val input = sc.textFile(args(0));
+
+    // word count per file
     val wc = input.flatMap(line => line.split(" ")).map(word =>(word,1));
-    val preOutput = wc.reduceByKey(_ + _);
+    val preWordOutput = wc.reduceByKey(_ + _);
     // switch key and value, sort by value, switch key and value back. sorted in ascending order
-    val output = preOutput.map(item => item.swap).sortByKey(true).map(item => item.swap);
-    output.saveAsTextFile(args(1));
+    val wordOutput = preWordOutput.map(item => item.swap).sortByKey(true).map(item => item.swap);
+    wordOutput.saveAsTextFile(args(1));
+
+    // word count per sentence
+    val sentCount = input.flatMap(line => line.split('.')).map(sentence => (sentence, (sentence.split(" ")).length));
+    val preSentOutput = sentCount.reduceByKey(_+_);
+    // switch key and value, sort by value, switch key and value back. sorted in ascending order
+    val sentOutput = preSentOutput.map(item => item.swap).sortByKey(true).map(item => item.swap);
+    sentOutput.saveAsTextFile(args(2));
   }
 
 }
